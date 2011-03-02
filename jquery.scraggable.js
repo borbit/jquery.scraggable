b9cb2f7438bfe5879afb9b4566f0b21dbed1ddb0
Serge Borbit <serge.borbit@gmail.com>
2011-03-01 23:02:11 -0800

    Now objects can be dragged to any sides in FF and Chrome, in Opera only to top or bottom sides. Implemented sensitivity option and containment(parent) option.

--- jquery.scraggable.js
+++ jquery.scraggable.js
@@ -1,33 +1,93 @@
-(function($) {
+(function($, undefined) {
 
-if (window.document.addEventListener) {
-    window.document.addEventListener('mousewheel', scrag, false);
-}
-
-function scrag(event) {
-    var deltaX = 0;
-    var deltaY = 0;
-
-    if (event.wheelDeltaX !== undefined) {
-        deltaX = event.wheelDeltaX;
-    }
-    if (event.wheelDeltaY !== undefined) {
-        deltaY = event.wheelDeltaY;
-    }
-
-    $(elements).each(function(index, element) {
-        element.css({
-            top: element.position().top + deltaY,
-            left: element.position().left + deltaX
-        });
-    });
-
-}
-
-var elements = [];
+var defaultOptions = {
+    axis: false,
+    sensitivity: 1,
+    containment: 'parent'
+};
 
 $.fn.scraggable = function(options) {
-     elements.push($(this));
+    options = $.extend(defaultOptions, options);
+
+    if (options.parent === undefined) {
+        options.parent = window.document;
+    }
+
+    if (options.parent.addEventListener) {
+        options.parent.addEventListener('mousewheel', onmousewhell, false);
+        options.parent.addEventListener('DOMMouseScroll', onmousewhell, false);
+    }
+
+    var element = $(this);
+    var width = element.outerWidth();
+    var height = element.outerHeight();
+    var position = element.position();
+    var bounds = false;
+    
+    if (options.containment == 'parent') {
+        bounds = {
+            top: 0, left: 0,
+            bottom: $(options.parent).height(),
+            right: $(options.parent).width()
+        };
+    }
+    
+
+    function onmousewhell(event) {
+        var wheelDelta = getWheelDelta(event);
+
+        position = fixNewPosition(
+            position.left + (wheelDelta[0] * options.sensitivity),
+            position.top + (wheelDelta[1] * options.sensitivity));
+
+        element.css({
+            top: position.top,
+            left: position.left
+        });
+    }
+
+    function fixNewPosition(newLeft, newTop) {
+        if(newLeft < bounds.left) {
+            newLeft = bounds.left;
+        }
+        if(newLeft + width > bounds.right) {
+            newLeft = bounds.right - width;
+        }
+        if(newTop < bounds.top) {
+            newTop = bounds.top;
+        }
+        if(newTop + height > bounds.bottom) {
+            newTop = bounds.bottom - height;
+        }
+
+        return {top: newTop, left: newLeft};
+    }
+
+    function getWheelDelta(event) {
+        // FIREFOX
+        if (event.axis != null) {
+            if (event.axis == event.HORIZONTAL_AXIS) {
+                return [-event.detail / 2, 0];
+            }
+            if (event.axis == event.VERTICAL_AXIS) {
+                return [0, -event.detail / 2];
+            }
+        }
+
+        // WEBKIT
+        if (event.wheelDeltaX != null &&
+            event.wheelDeltaY != null) {
+            return [event.wheelDeltaX / 60,
+                    event.wheelDeltaY / 60];
+        }
+
+        // OPERA
+        if (event.detail != null && event.axis == null) {
+            return [0, -event.detail / 2];
+        }
+
+        return [0, 0];
+    }
 };
 
 })(jQuery);
