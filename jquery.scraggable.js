96d722323772f5c518d09754ea049cf09e9370c4
Serge Borbit <serge.borbit@gmail.com>
2011-03-02 23:14:57 -0800

    Implemented functionality for "containment" and "parent" options. Needs to be refactored.

--- jquery.scraggable.js
+++ jquery.scraggable.js
@@ -1,44 +1,77 @@
 (function($, undefined) {
 
-var defaultOptions = {
-    axis: false,
-    sensitivity: 1,
-    containment: 'parent'
+$.fn.scraggable = function(options) {
+    options = $.extend({}, $.fn.scraggable.defaults, options);
+
+    return this.each(function() {
+        process($(this), options);
+    });
 };
 
-$.fn.scraggable = function(options) {
-    options = $.extend(defaultOptions, options);
+$.fn.scraggable.defaults = {
+    axis: false,
+    parent: false,
+    sensitivity: 1,
+    containment: false
+};
 
-    if (options.parent === undefined) {
-        options.parent = window.document;
-    }
-
-    if (options.parent.addEventListener) {
-        options.parent.addEventListener('mousewheel', onmousewhell, false);
-        options.parent.addEventListener('DOMMouseScroll', onmousewhell, false);
-    }
-
-    var element = $(this);
+function process(element, options) {
     var width = element.outerWidth();
     var height = element.outerHeight();
     var position = element.position();
-    var bounds = false;
-    
+    var offset = element.offset();
+    var parent = null;
+
     if (options.containment == 'parent') {
+        if (typeof(options.parent) == 'object' &&
+            options.parent.jquery !== undefined) {
+            parent = options.parent;
+        }
+        if (typeof(options.parent) == 'object' &&
+            options.parent.tagName !== undefined) {
+            parent = $(options.parent);
+        }
+        if (typeof(options.parent) == 'string') {
+            parent = $(options.parent);
+        }
+        if (options.parent == false) {
+            parent = element.parent();
+        }
+    }
+
+    if (options.containment == false ||
+        options.containment == 'document') {
+        parent = $(window.document);
+    }
+
+    if (parent.get(0).addEventListener) {
+        parent.get(0).addEventListener('mousewheel', onmousewhell, false);
+        parent.get(0).addEventListener('DOMMouseScroll', onmousewhell, false);
+    }
+
+    var bounds = false;
+
+    if (options.containment == 'document') {
         bounds = {
             top: 0, left: 0,
-            bottom: $(options.parent).height(),
-            right: $(options.parent).width()
+            bottom: parent.height(),
+            right: parent.width()
+        };
+    } else if (options.containment) {
+        var parentOffset = parent.offset();
+        bounds = {
+            top: parentOffset.top, left: parentOffset.left,
+            bottom: parentOffset.top + parent.outerHeight(),
+            right: parentOffset.left + parent.outerWidth()
         };
     }
-    
 
     function onmousewhell(event) {
         var wheelDelta = getWheelDelta(event);
 
-        position = fixNewPosition(
-            position.left + (wheelDelta[0] * options.sensitivity),
-            position.top + (wheelDelta[1] * options.sensitivity));
+        position = fixPosition(
+            offset.left + (wheelDelta[0] * options.sensitivity),
+            offset.top + (wheelDelta[1] * options.sensitivity));
 
         element.css({
             top: position.top,
@@ -46,21 +79,29 @@ $.fn.scraggable = function(options) {
         });
     }
 
-    function fixNewPosition(newLeft, newTop) {
-        if(newLeft < bounds.left) {
+    function fixPosition(newLeft, newTop) {
+        if (newLeft < bounds.left) {
             newLeft = bounds.left;
         }
-        if(newLeft + width > bounds.right) {
+        if (newLeft + width > bounds.right) {
             newLeft = bounds.right - width;
         }
-        if(newTop < bounds.top) {
+        if (newTop < bounds.top) {
             newTop = bounds.top;
         }
-        if(newTop + height > bounds.bottom) {
+        if (newTop + height > bounds.bottom) {
             newTop = bounds.bottom - height;
         }
 
-        return {top: newTop, left: newLeft};
+        var newPos = {
+            top: position.top + newTop - offset.top,
+            left: position.left + newLeft - offset.left
+        };
+
+        offset.top = newTop;
+        offset.left = newLeft;
+
+        return newPos;
     }
 
     function getWheelDelta(event) {
@@ -73,14 +114,10 @@ $.fn.scraggable = function(options) {
                 return [0, -event.detail / 2];
             }
         }
-
         // WEBKIT
-        if (event.wheelDeltaX != null &&
-            event.wheelDeltaY != null) {
-            return [event.wheelDeltaX / 60,
-                    event.wheelDeltaY / 60];
+        if (event.wheelDeltaX != null && event.wheelDeltaY != null) {
+            return [event.wheelDeltaX / 60, event.wheelDeltaY / 60];
         }
-
         // OPERA
         if (event.detail != null && event.axis == null) {
             return [0, -event.detail / 2];
@@ -88,6 +125,6 @@ $.fn.scraggable = function(options) {
 
         return [0, 0];
     }
-};
+}
 
 })(jQuery);
