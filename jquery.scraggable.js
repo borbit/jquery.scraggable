d1297709327883d45cb038c7790c4d443a196327
Serge Borbit <serge.borbit@gmail.com>
2011-03-04 00:12:36 -0800

    Moved all code to the class (with using of prototype).

--- jquery.scraggable.js
+++ jquery.scraggable.js
@@ -4,7 +4,7 @@ $.fn.scraggable = function(options) {
     options = $.extend({}, $.fn.scraggable.defaults, options);
 
     return this.each(function() {
-        process($(this), options);
+        new Scraggable($(this), options);
     });
 };
 
@@ -16,131 +16,143 @@ $.fn.scraggable.defaults = {
     containment: false
 };
 
-function process(element, options) {
-    var width = element.outerWidth(),
-        height = element.outerHeight(),
-        position = element.position(),
-        offset = element.offset(),
-        parent = null;
+function Scraggable(element, options) {
+    this.element = element;
+    this.options = options;
+
+    this.width = this.element.outerWidth();
+    this.height = this.element.outerHeight();
+    this.position = this.element.position();
+    this.offset = this.element.offset();
+    this.parent = null;
 
     if (options.containment == 'parent') {
-        if (typeof(options.parent) == 'object' &&
-            options.parent.jquery !== undefined) {
-            parent = options.parent;
+        if (typeof(this.options.parent) == 'object' &&
+            this.options.parent.jquery !== undefined) {
+            this.parent = this.options.parent;
         }
-        if (typeof(options.parent) == 'object' &&
-            options.parent.tagName !== undefined) {
-            parent = $(options.parent);
+        if (typeof(this.options.parent) == 'object' &&
+            this.options.parent.tagName !== undefined) {
+            this.parent = $(this.options.parent);
         }
-        if (typeof(options.parent) == 'string') {
-            parent = $(options.parent);
+        if (typeof(this.options.parent) == 'string') {
+            this.parent = $(this.options.parent);
         }
-        if (options.parent == false) {
-            parent = element.parent();
+        if (this.options.parent == false) {
+            this.parent = this.element.parent();
         }
     } else {
-        parent = $(window.document);
+        this.parent = $(window.document);
     }
 
-    if (parent.get(0).addEventListener) {
-        parent.get(0).addEventListener('mousewheel', onmousewhell, false);
-        parent.get(0).addEventListener('DOMMouseScroll', onmousewhell, false);
+    this.bounds = this.getBounds();
+
+    if (this.parent.get(0).addEventListener) {
+        this.parent.get(0).addEventListener('mousewheel', onmousewhell, false);
+        this.parent.get(0).addEventListener('DOMMouseScroll', onmousewhell, false);
     }
 
-    var bounds = false;
-
-    if (options.containment == 'document') {
-        bounds = {
-            left: 0, top: 0,
-            right: parent.width(),
-            bottom: parent.height()
-        };
-    } else if (options.containment == 'parent') {
-        var parentOffset = parent.offset();
-        bounds = {
-            left: parentOffset.left, top: parentOffset.top,
-            right: parentOffset.left + parent.outerWidth(),
-            bottom: parentOffset.top + parent.outerHeight()
-        };
-    } else if ($.isArray(options.containment)) {
-        bounds = {
-            left: options.containment[0],
-            top: options.containment[1],
-            right: options.containment[2],
-            bottom: options.containment[3]
-        };
-    }
+    var self = this;
 
     function onmousewhell(event) {
-        var wheelDelta = getWheelDelta(event);
-        var newOffset = {
-            left: offset.left + (wheelDelta[0] * options.sensitivity),
-            top: offset.top + (wheelDelta[1] * options.sensitivity)
-        };
-
-        if (bounds) {
-            newOffset = fixOffset(newOffset);
-        }
-
-        setPosition(newOffset);
-    }
-
-    function fixOffset(newOffset) {
-        if (newOffset.left < bounds.left) {
-            newOffset.left = bounds.left;
-        }
-        if (newOffset.left + width > bounds.right) {
-            newOffset.left = bounds.right - width;
-        }
-        if (newOffset.top < bounds.top) {
-            newOffset.top = bounds.top;
-        }
-        if (newOffset.top + height > bounds.bottom) {
-            newOffset.top = bounds.bottom - height;
-        }
-        return newOffset;
-    }
-
-    function setPosition(newOffset) {
-        var diffTop = newOffset.top - offset.top;
-        var diffLeft = newOffset.left - offset.left;
-        
-        if (!options.axis || options.axis == 'x') {
-            position.left = position.left + (options.inverted ? diffTop : diffLeft);
-        }
-        if (!options.axis || options.axis == 'y') {
-            position.top = position.top + (options.inverted ? diffLeft : diffTop);
-        }
-
-        element.css({
-            top: position.top,
-            left: position.left
-        });
-
-        offset = newOffset;
-    }
-
-    function getWheelDelta(event) {
-        // FIREFOX
-        if (event.axis != null) {
-            if (event.axis == event.HORIZONTAL_AXIS) {
-                return [-event.detail / 2, 0];
-            }
-            if (event.axis == event.VERTICAL_AXIS) {
-                return [0, -event.detail / 2];
-            }
-        }
-        // WEBKIT
-        if (event.wheelDeltaX != null && event.wheelDeltaY != null) {
-            return [event.wheelDeltaX / 60, event.wheelDeltaY / 60];
-        }
-        // OPERA
-        if (event.detail != null && event.axis == null) {
-            return [0, -event.detail / 2];
-        }
-
-        return [0, 0];
+        self.processMouseWhell(event);
     }
 }
+
+Scraggable.prototype.processMouseWhell = function(event) {
+    var wheelDelta = this.getWheelDelta(event);
+    var newOffset = {
+        left: this.offset.left + (wheelDelta[0] * this.options.sensitivity),
+        top: this.offset.top + (wheelDelta[1] * this.options.sensitivity)
+    };
+
+    if (this.bounds) {
+        newOffset = this.fixOffset(newOffset);
+    }
+
+    this.setPosition(newOffset);
+};
+
+Scraggable.prototype.fixOffset = function(newOffset) {
+    if (newOffset.left < this.bounds.left) {
+        newOffset.left = this.bounds.left;
+    }
+    if (newOffset.left + this.width > this.bounds.right) {
+        newOffset.left = this.bounds.right - this.width;
+    }
+    if (newOffset.top < this.bounds.top) {
+        newOffset.top = this.bounds.top;
+    }
+    if (newOffset.top + this.height > this.bounds.bottom) {
+        newOffset.top = this.bounds.bottom - this.height;
+    }
+    return newOffset;
+};
+
+Scraggable.prototype.setPosition = function(newOffset) {
+    var diffTop = newOffset.top - this.offset.top;
+    var diffLeft = newOffset.left - this.offset.left;
+
+    if (!this.options.axis || this.options.axis == 'x') {
+        this.position.left += (this.options.inverted ? diffTop : diffLeft);
+    }
+    if (!this.options.axis || this.options.axis == 'y') {
+        this.position.top += (this.options.inverted ? diffLeft : diffTop);
+    }
+
+    this.element.css({
+        top: this.position.top,
+        left: this.position.left
+    });
+
+    this.offset = newOffset;
+};
+
+Scraggable.prototype.getWheelDelta = function(event) {
+    // FIREFOX
+    if (event.axis != null) {
+        if (event.axis == event.HORIZONTAL_AXIS) {
+            return [-event.detail / 2, 0];
+        }
+        if (event.axis == event.VERTICAL_AXIS) {
+            return [0, -event.detail / 2];
+        }
+    }
+    // WEBKIT
+    if (event.wheelDeltaX != null && event.wheelDeltaY != null) {
+        return [event.wheelDeltaX / 60, event.wheelDeltaY / 60];
+    }
+    // OPERA
+    if (event.detail != null && event.axis == null) {
+        return [0, -event.detail / 2];
+    }
+
+    return [0, 0];
+};
+
+Scraggable.prototype.getBounds = function() {
+    if (this.options.containment == 'document') {
+        return {
+            left: 0, top: 0,
+            right: this.parent.width(),
+            bottom: this.parent.height()
+        };
+    } else if (this.options.containment == 'parent') {
+        var parentOffset = this.parent.offset();
+        return {
+            left: parentOffset.left, top: parentOffset.top,
+            right: parentOffset.left + this.parent.outerWidth(),
+            bottom: parentOffset.top + this.parent.outerHeight()
+        };
+    } else if ($.isArray(this.options.containment)) {
+        return {
+            left: this.options.containment[0],
+            top: this.options.containment[1],
+            right: this.options.containment[2],
+            bottom: this.options.containment[3]
+        };
+    }
+    return false;
+};
 
 })(jQuery);
